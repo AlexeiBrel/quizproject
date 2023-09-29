a1c048253531bf48112aee30b672901d20c9a88a
Alexey Brel <brel.2000@mail.ru>
2023-09-29 22:43:16 +0300

    bug fix

--- src/api/Service.js
+++ src/api/Service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default class Service {
     static async getAll() {
-        const response = await axios.get('/src/data/products.json');
+        const response = await axios.get('../src/data/products.json');
         return response.data
     }
 }
